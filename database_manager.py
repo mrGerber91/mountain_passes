001be834834b008cfb1b19f34c5fe9445a4a7de4
mrGerber91 <lalkakapalka3@gmail.com>
2024-05-07 21:58:04 +0300

    Project overhaul

--- database_manager.py
+++ database_manager.py
@@ -44,3 +44,53 @@ class DataBaseManager:
         except Exception as e:
             self.conn.rollback()
             print(f"Unable to add pereval: {e}")
+
+    def add_image(self, title):
+        # Метод для добавления нового изображения в базу данных
+        try:
+            sql_query = sql.SQL("INSERT INTO pereval_images (title) VALUES (%s)")
+            self.cur.execute(sql_query, (title,))
+            self.conn.commit()
+            print("Image added successfully")
+        except Exception as e:
+            self.conn.rollback()
+            print(f"Unable to add image: {e}")
+
+    def add_user(self, email, fam, name, otc):
+        # Метод для добавления нового пользователя в базу данных
+        try:
+            sql_query = sql.SQL("INSERT INTO users (email, fam, name, otc) VALUES (%s, %s, %s, %s)")
+            self.cur.execute(sql_query, (email, fam, name, otc))
+            self.conn.commit()
+            print("User added successfully")
+        except Exception as e:
+            self.conn.rollback()
+            print(f"Unable to add user: {e}")
+
+    def moderate_pereval(self, pereval_id, status):
+        # Метод для изменения статуса модерации перевала
+        try:
+            sql_query = sql.SQL("UPDATE pereval_added SET status = %s WHERE id = %s")
+            self.cur.execute(sql_query, (status, pereval_id))
+            self.conn.commit()
+            print("Pereval status updated successfully")
+        except Exception as e:
+            self.conn.rollback()
+            print(f"Unable to update pereval status: {e}")
+
+    def close_connection(self):
+        # Метод для закрытия соединения с базой данных
+        self.cur.close()
+        self.conn.close()
+        print("Connection to the database closed")
+
+    def get_all_perevals(self):
+        # Метод для получения списка всех перевалов из базы данных
+        try:
+            self.cur.execute("SELECT * FROM pereval_added")
+            perevals = self.cur.fetchall()
+            print("All perevals retrieved successfully")
+            return perevals
+        except Exception as e:
+            print(f"Unable to retrieve all perevals: {e}")
+            return None
