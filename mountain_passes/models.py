0d731336cef4dcc96bac75e8a1b53ba88357356b
mrGerber91 <lalkakapalka3@gmail.com>
2024-05-07 22:04:38 +0300

    Add models

---
+++ mountain_passes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class PerevalAdded(models.Model):
+    date_added = models.DateTimeField()
+    beauty_title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    other_titles = models.CharField(max_length=100)
+    connect = models.TextField()
+    add_time = models.DateTimeField()
+    winter_level = models.CharField(max_length=10)
+    summer_level = models.CharField(max_length=10)
+    autumn_level = models.CharField(max_length=10)
+    spring_level = models.CharField(max_length=10)
+    coord_id = models.IntegerField()
+    user_id = models.IntegerField()
+    status = models.CharField(max_length=20)
+
+class PerevalImages(models.Model):
+    title = models.CharField(max_length=100)
+
+class Users(models.Model):
+    email = models.EmailField()
+    fam = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    otc = models.CharField(max_length=100)
